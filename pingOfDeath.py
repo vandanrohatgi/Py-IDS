fd679d1582d2f1855d9d629e4c10c7066c0ec16e
vandan rohatgi <43648786+vandanrohatgi@users.noreply.github.com>
2020-07-12 11:16:51 +0530

    added module to detect ping of death

---
+++ pingOfDeath.py
@@ -0,0 +1,37 @@
+
+from scapy.layers.inet import IP
+
+# class to detect ping of death
+
+
+class PingOfDeath:
+    def __init__(self):
+        # dictionary to store size of all icmp packets from different IPs
+        self.pod = {}
+        # if size of a packet from a single IP is more than maximum alowed size
+        self.sizethreshold = 65000
+        self.podAttacked = False
+        self.WARNING = '\033[91m'
+        self.BOLD = '\033[1m'
+
+    def podDetect(self, pkt):
+        if not self.podAttacked:
+            if IP in pkt:
+                ip = pkt[IP].src
+                # check if the protocol of packets is ICMP(ping packet)
+                if pkt[IP].proto == 1:
+                    if ip != None:
+                        # check if ip address is already inside dictionary
+                        if ip not in self.pod.keys():
+                            self.pod[ip] = {'size': len(pkt)}
+                        else:
+                            # else just update the size of packets received from that IP
+                            self.pod[ip]['size'] = self.pod[ip]['size'] + \
+                                len(pkt)
+                        # finally iterate over all IP addresses and see if anyone crosses the threshold
+                        for target in self.pod.keys():
+                            if self.pod[target]['size'] > self.sizethreshold:
+                                print(
+                                    f'{self.WARNING}{self.BOLD}Warning! You may have received a possible ping of death from IP'+target)
+                                self.podAttacked = True
+                                break
--- start.py
+++ start.py
@@ -1,17 +1,20 @@
 from portScans import ScanDetector
+from pingOfDeath import PingOfDeath
 from scapy.sendrecv import sniff
 import sys
 
-interface=input("Please enter the interface you want to run Py-IDS on:")
 print('Initializing...')
 
+interface=input("Please enter the interface you want to run Py-IDS on:")
+
 scanObj=ScanDetector()
+podObj=PingOfDeath()
 
 print('PY-IDS is online and looking for attacks')
 
-try:
-    # sniff the packets and send them to function oneForAll
-    sniff(iface=interface,prn=scanObj.oneForAll)
-except:
-    print('An error occured. Exiting...')
-    sys.exit()
+def main(pkt):
+    scanObj.oneForAll(pkt)
+    podObj.podDetect(pkt)
+
+#sniff the packets and send them to functions to detect the attacks
+sniff(iface=interface,prn=main)
