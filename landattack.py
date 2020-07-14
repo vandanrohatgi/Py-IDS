47a458bdc45df1984699b63e1e6aa92b08d6a6f4
vandan rohatgi <43648786+vandanrohatgi@users.noreply.github.com>
2020-07-14 09:44:41 +0530

    added new attacks
    added land attack, a failure at attempt of detecting spoofed IP address and updated start.py

---
+++ landattack.py
@@ -0,0 +1,17 @@
+from scapy.layers.inet import IP, TCP
+
+# function to detect land attack
+
+
+def landAttack(pkt):
+    # check for TCP and IP layers in packet
+    if TCP in pkt:
+        if IP in pkt:
+            srcIp = pkt[IP].src
+            dstIp = pkt[IP].dst
+            srcPort = pkt[TCP].sport
+            dstPort = pkt[TCP].dport
+            if dstPort != None and srcPort != None:
+                # if both source port and IP are same then alert
+                if srcIp == dstIp and srcPort == dstPort:
+                    print(f'You just received a land attack packet from IP:'+str(srcIp))
