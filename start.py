7d5f9e29cdc539374398db1b3f2cceb7dcb20519
vandan rohatgi <43648786+vandanrohatgi@users.noreply.github.com>
2020-07-16 11:10:20 +0530

    added smurf attack detection module

--- start.py
+++ start.py
@@ -2,6 +2,7 @@ from portScans import ScanDetector
 from pingOfDeath import PingOfDeath
 from landattack import landAttack
 #from IPspoof import spoofCheck
+#from smurf import Smurf
 from scapy.sendrecv import sniff
 import sys
 
@@ -11,6 +12,7 @@ interface=input("Please enter the interface you want to run Py-IDS on:")
 
 scanObj=ScanDetector()
 podObj=PingOfDeath()
+#smurfobj=Smurf()
 
 print('PY-IDS is online and looking for attacks')
 
@@ -18,6 +20,7 @@ def main(pkt):
     scanObj.oneForAll(pkt)
     podObj.podDetect(pkt)
     landAttack(pkt)
+    #smurfobj.detectSmurf(pkt)
     #spoofCheck(pkt)
 
 #sniff the packets and send them to functions to detect the attacks
